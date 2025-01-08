3885c24e463079ee5f8f20a87ae550fe9f4361dc
R.M Dilshan Savinda Rathnayaka <dilshansavinda83@gmail.com>
2025-01-08 12:26:42 +0530

    Updated button components for Login and Register

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import {Container,Row,Button} from 'reactstrap';
+import { NavLink,Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import './Header.css';
+
+const nav_links=[
+    {
+        path:'/home'  ,
+        display:'Home'
+
+    },
+    {
+        path:'/about'  ,
+        display:'About'
+
+    },
+    {
+        path:'/tours'  ,
+        display:'Tours'
+
+    },
+   
+
+    
+]
 
 const Header = () =>{
-    return <div>Header </div>
+    return <header className="header">
+        <Container>
+            <Row>
+                <div className="nav_wrapper d-flex align-items-center justify-content-between"div>
+
+                    {/*==========logo==========*/}
+                    <div className="logo">
+                        <img src={logo} alt=""/>
+                        </div>
+
+                    {/*==========End==========*/}
+                    {/*=========Menu start ==========*/}
+                    <div className="Navigation">
+                        <ul className="Menu d-flex align-items-center gap-5">
+                            {
+                                nav_links.map((item,index)=>(
+                                    <li className="nav_item" key={index}>
+                                        <NavLink 
+                                        to={item.path} 
+                                        className={navClass=>
+                                        navClass.isActive ? "active_link" : ""
+                                        }
+                                        >
+                                        {item.display}
+                                        </NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    {/*==========Menu End==========*/}
+                    <div className="nav_right d-flex align-items-center gap-4">
+                    <div className="nav_btns d-flex align-items-center gap-4">
+                    <button className="btn secondary__btn"><Link to='/Login'>Login</Link></button>
+                    <button className="btn primary__btn"><Link to='/Register'>Register</Link></button>
+                    </div>
+
+                    <span className="mobile_menu">
+                    <i class="ri-menu-line"></i>
+                    </span>
+                  </div>
+                </div>
+            </Row>
+        </Container>
+        </header>
 };
 
 export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import '../styles/home.css';
 
-const Home = () =>{
-    return <div> Home </div>
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import Subtitle from '../shared/Subtitle';
+import SearchBar from "../shared/SearchBar";
+
+const Home = () => {
+  return <>
+
+  {/*===========hero section start here================*/}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+          <div className="hero_content">
+            <div className="hero_subtitle d-flex align-items-center">
+              <Subtitle Subtitle={ 'Know Before You Go' }/>
+              <img src={worldImg} alt="" />
+            </div>
+            <h1>Traveling opens the door to creating <span className="highlight">memories</span></h1>
+            <p>Discover the World: Your Adventure Awaits
+Unique Tours Explore hidden gems with expert guides. Dive into culture, history, and natural beauty.
+Adventure Packages Thrill-seekers, gear up! Experience mountain climbing, scuba diving, and more.
+Experiential Experiences Engage in local traditions. Participate in workshops, culinary classes, and craft sessions.</p>
+            </div>
+          </Col>
+          <Col lg='2'>
+           <div className="hero_img-box">
+            <img src={heroImg} alt="" />
+           </div>
+          </Col>
+          <Col lg='2'>
+           <div className="hero_img-box mt-4">
+            <video src={heroVideo} alt="" controls/>
+           </div>
+          </Col>
+          <Col lg='2'>
+           <div className="hero_img-box mt-5">
+            <img src={heroImg02} alt="" />
+           </div>
+          </Col>
+          <SearchBar/>
+        </Row>
+      </Container>
+    </section>
+    {/*===========hero section ends here================*/}
+    <section>
+        <Container>
+            <Row>
+                <Col lg='3'>
+                <h5 className="services_subtitle">What we serve</h5>
+                <h2 className="services_title">We offer our best services</h2>
+                </Col>
+            </Row>
+
+        </Container>
+    </section>
+  </>
 };
 
 export default Home;
